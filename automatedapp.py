65814f4347fb70daee7cd59d6fe1ff79ac367dd9
splawrence <sp_lawrence@outlook.com>
2021-04-09 12:21:15 -0500

    Update automatedapp.py

--- automatedapp.py
+++ automatedapp.py
@@ -4,42 +4,73 @@ from datetime import datetime, timedelta
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
-import smtplib
-import ssl
 import sys
 
 # sets the environmental variables
-active_profile = "prod"
+# active_profile = "prod"
+active_profile = "dev"
 
-# Mr. Computer, please start here ;)
+# scheduleing details
+# even or odd run day option. by default this is 9
+# run_day = 9
+run_day = 10
+
+# weekday run options
+# weekday_run_hour = 17
+weekday_run_hour = 17
+weekday_timeslot1 = "5:00pm-5:30pm"
+weekday_timeslot2 = "5:30pm-6:00pm"
+
+# weekend run options
+# weekend_run_hour = 11
+weekend_run_hour = 12
+weekend_timeslot1 = "11:00am-11:30am"
+weekend_timeslot2 = "11:30am-12:00pm"
+
+# days in advance to run
+time_offset = timedelta(days=2)
+
+# slot details
+# searched event details
+category_number = "8980"
+location_number = "701"
+
+# input event details
+category_description = "Open Schedules (i.e., pool, gym, gymnastics)"
+studio_description = "Meter Pool"
+open_slots_number = "1"
+event_title = "Lap/Fitness Lane Reservation"
+
 def main():
     # scheduling logic
     # run every other day
     # if registration is for weekend, run at 11:30 for 11-12
     # else run at 5:30 for 5-6
-    if (datetime.now().date() - datetime(2021, 2, 9).date()).days % 2 == 0:
+    if (datetime.now().date() - datetime(2021, 2, run_day).date()).days % 2 == 0:
+
         # set up time slots
         week_no = datetime.today().weekday()
-        first_timeslot = Event()
-        second_timeslot = Event()
+
         # check if registration is for a weekend
         if week_no < 3 or week_no > 4:
             # if run occurs at 5pm
-            if datetime.now().hour == 17:
-                first_timeslot.create_from_time("5:00pm-5:30pm")
-                second_timeslot.create_from_time("5:30pm-6:00pm")
-                # set time list
-                input_events = [first_timeslot, second_timeslot]
-                do_stuff(input_events)
-
+            if datetime.now().hour == weekday_run_hour:
+                setup_slots(weekday_timeslot1, weekday_timeslot2)
         else:
             # if run occurs at 11am
-            if datetime.now().hour == 11:
-                first_timeslot.create_from_time("11:00am-11:30am")
-                second_timeslot.create_from_time("11:30am-12:00pm")
-                # set time list
-                input_events = [first_timeslot, second_timeslot]
-                do_stuff(input_events)
+            if datetime.now().hour == weekend_run_hour:
+                setup_slots(weekend_timeslot1, weekend_timeslot2)
+
+def setup_slots(time_slot1, time_slot2):
+        first_event = Event()
+        second_event = Event()
+
+        first_event.create_from_time(time_slot1)
+        second_event.create_from_time(time_slot2)
+
+        # set time list
+        input_events = [first_event, second_event]
+        do_stuff(input_events)
 
 def do_stuff(input_events):
     person_list = []
@@ -51,12 +82,10 @@ def do_stuff(input_events):
         # local development
         file = open("login.csv", "r")
 
-
     for login in file:
         person_list.append(login.split(","))
     file.close()
 
-    event_list = []
     event_list = get_future_events()
 
     desired_event_list = []
@@ -64,20 +93,14 @@ def do_stuff(input_events):
         desired_event_list.append(
             find_one_event_from_inputs(event_list, input_event))
 
-    error_message = []
-
     if desired_event_list[0] is not None and desired_event_list[1] is not None:
         print("Slots found. Commencing registration.")
         for person in person_list:
-            error_message.append(make_reservations(
-                person[0], person[1], desired_event_list))
-        # check for errors
-        if len(error_message) > 0:
-           sys.exit(1)
+            make_reservations(person[0], person[1], desired_event_list)
     else:
         print("Error: Slots could not be found")
-        print("input event date was: " + str(input_event.date))
-        print("actual event date was: " + str(event_list[0].date))
+        print("Input event date was: " + str(input_event.date))
+        print("Actual event date was: " + str(event_list[0].date))
         sys.exit(404)
 
 def login(driver, email, password):
@@ -91,7 +114,7 @@ def submit(driver):
     submit_element = driver.find_element_by_name("submit")
     submit_element.click()
 
-def is_waitlisted(driver, time_stamp):
+def check_for_waitlisted(driver, time_stamp):
         # check that there is an availible slot
         if(driver.find_element_by_name("action").get_attribute("value") == "waitlist"):
             print("Waitlisted: " + time_stamp)
@@ -110,48 +133,35 @@ def make_reservations(email, password, desired_event_list):
         # run on windows chromedriver.exe
         driver = webdriver.Chrome(options=chrome_options)
 
-    error_message = []
     is_logged_in = False
     for event in desired_event_list:
-        time_stamp = (
-            str(email)
-            + str(" for ")
-            + str(event.month)
-            + " "
-            + str(event.date)
-            + " "
-            + str(event.time)
-        )
+        slot_info = (str(email) + str(" for ") + str(event.month) + " " + str(event.date) + " " + str(event.time))
         driver.get(event.sign_up_url)
         try:
             if is_logged_in is False:
                 # login
                 login(driver, email, password)
 
-                # check is waitlisted
-                is_waitlisted(driver, time_stamp)
-                
+                # check waitlisted status
+                check_for_waitlisted(driver, slot_info)
+
                 # reserve slot
                 submit(driver)
                 is_logged_in = True
             else:
                 # check is waitlisted
-                is_waitlisted(driver, time_stamp)
+                check_for_waitlisted(driver, slot_info)
 
                 # reserve slot
                 submit(driver)
         except NoSuchElementException:
-            print("Error: Failed to register: " + time_stamp)
-            error_message.append("Failed to register: " + time_stamp)
+            print("Error: Failed to register: " + slot_info)
 
     driver.close()
-    return error_message
 
 def get_future_events():
-    category_number = "8980"
-    location_number = "701"
-    event_string_list = []
-    begin_date_time = datetime.now() + timedelta(days=2)
+    event_str_list = []
+    begin_date_time = datetime.now() + time_offset
     end_date_time = begin_date_time
     # beginDate
     begintime = time.mktime(begin_date_time.timetuple())
@@ -160,8 +170,8 @@ def get_future_events():
     endatetimeime = time.mktime(end_date_time.timetuple())
     endatetimeime_str = str(int(endatetimeime))
 
-    resp = requests.get(
-        "https://groupexpro.com/schedule/embed/json.php?schedule&instructor_id=true&format=jsonp&a=110&location="
+    event_list = []
+    url = str("https://groupexpro.com/schedule/embed/json.php?schedule&instructor_id=true&format=jsonp&a=110&location="
         + location_number
         + "&category="
         + category_number
@@ -169,27 +179,30 @@ def get_future_events():
         + begintime_str
         + "&end="
         + endatetimeime_str
-        + "&callback=&_="
-    )
-    if resp.status_code != 200:
+        + "&callback=&_=")
+    response = requests.get(url)
+
+    if response.status_code != 200:
         # this is bad
-        print("Response code: " + str(resp.status_code))
-        print("Unexpected error")
+        print("Response code: " + str(response.status_code))
     else:
         print(
             "Response received from https://groupexpro.com: "
-            + str(resp.status_code)
+            + str(response.status_code)
             + " OK"
         )
-        event_string_list = format_response_body(resp.text)
+        # remove code and invalid characters from response
+        str_list = format_response_body(response.text)
+        # loop through each string and format the columns
+        event_str_list = format_columns(str_list)
+        # loop through each event string and create and event from each
+        event_list = create_event_from_string(event_str_list)
 
-    event_list = format_column_string_to_column_list(event_string_list)
-    event_list = convert_event_str_list_to_event_list(event_list)
-    print("There are: " + str(len(event_list)) + " events to pick from. Yay!")
+        print("There are: " + str(len(event_list)) + " events to pick from. Yay!")
+
     return event_list
 
-
-def format_column_string_to_column_list(event_string_list):
+def format_columns(event_string_list):
     event_list = []
     # setup event string list
     for row_string in event_string_list:
@@ -202,7 +215,6 @@ def format_column_string_to_column_list(event_string_list):
             x = x + 1
         event_list.append(column_list)
     return event_list
-
 
 def format_response_body(response_body):
     # format payload
@@ -228,8 +240,7 @@ def format_response_body(response_body):
     # create a list using pipes as delimiter
     return list(json_pay_load.split("|"))
 
-
-def convert_event_str_list_to_event_list(event_str_list):
+def create_event_from_string(event_str_list):
     event_list = []
     for event_str in event_str_list:
         new_event = Event()
@@ -296,15 +307,14 @@ class Event:
         return self
 
     def create_from_time(self, time):
-        self.category = "Open Schedules (i.e., pool, gym, gymnastics)"
-        self.studio = "Meter Pool"
-        self.open_slots = "1"
-        self.title = "Lap/Fitness Lane Reservation"
+        self.category = category_description
+        self.studio = studio_description
+        self.open_slots = open_slots_number
+        self.title = event_title
         self.time = time
-        date = datetime.now() + timedelta(days=2)
+        date = datetime.now() + time_offset
         self.date = str(date.day)
         return self
-
 
 if __name__ == "__main__":
     main()
