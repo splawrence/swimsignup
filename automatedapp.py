f297ee2a600d7b49a7c0e7fedb0d65757d97e334
splawrence <sp_lawrence@outlook.com>
2021-04-09 20:22:40 -0500

    Update automatedapp.py

--- automatedapp.py
+++ automatedapp.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
 import sys
+import json
 
 # sets the environmental variables
 active_profile = "prod"
@@ -60,11 +61,11 @@ def main():
                 setup_slots(weekend_timeslot1, weekend_timeslot2)
 
 def setup_slots(time_slot1, time_slot2):
-        first_event = Event()
-        second_event = Event()
+        first_event = event()
+        second_event = event()
 
-        first_event.create_from_time(time_slot1)
-        second_event.create_from_time(time_slot2)
+        first_event.create_user_event(time_slot1)
+        second_event.create_user_event(time_slot2)
 
         # set time list
         input_events = [first_event, second_event]
@@ -84,21 +85,14 @@ def do_stuff(input_events):
         person_list.append(login.split(","))
     file.close()
 
-    event_list = get_future_events()
+    event_list = find_events(input_events)
 
-    desired_event_list = []
-    for input_event in input_events:
-        desired_event_list.append(
-            find_one_event_from_inputs(event_list, input_event))
-
-    if desired_event_list[0] is not None and desired_event_list[1] is not None:
+    if len(event_list) > 0:
         print("Slots found. Commencing registration.")
         for person in person_list:
-            make_reservations(person[0], person[1], desired_event_list)
+            make_reservations(person[0], person[1], event_list)
     else:
         print("Error: Slots could not be found")
-        print("Input event date was: " + str(input_event.date))
-        print("Actual event date was: " + str(event_list[0].date))
         sys.exit(404)
 
 def login(driver, email, password):
@@ -157,8 +151,7 @@ def make_reservations(email, password, desired_event_list):
 
     driver.close()
 
-def get_future_events():
-    event_str_list = []
+def find_events(input_events):
     begin_date_time = datetime.now() + time_offset
     end_date_time = begin_date_time
     # beginDate
@@ -170,148 +163,86 @@ def get_future_events():
 
     event_list = []
     url = str("https://groupexpro.com/schedule/embed/json.php?schedule&instructor_id=true&format=jsonp&a=110&location="
-        + location_number
-        + "&category="
-        + category_number
-        + "&start="
-        + begintime_str
-        + "&end="
-        + endatetimeime_str
-        + "&callback=&_=")
+        + location_number + "&category=" + category_number + "&start=" + begintime_str + "&end=" + endatetimeime_str + "&callback=&_=")
     response = requests.get(url)
+    desired_event_list = []
 
-    if response.status_code != 200:
-        # this is bad
-        print("Response code: " + str(response.status_code))
-    else:
-        print(
-            "Response received from https://groupexpro.com: "
-            + str(response.status_code)
-            + " OK"
-        )
-        # remove code and invalid characters from response
-        str_list = format_response_body(response.text)
-        # loop through each string and format the columns
-        event_str_list = format_columns(str_list)
+    if response.status_code == 200:
+        print("Response received from https://groupexpro.com: " + str(response.status_code) + " OK")
+        # get json from response
+        event_str_list = response_mapper(response.text)
+
         # loop through each event string and create and event from each
         event_list = create_event_from_string(event_str_list)
 
-        print("There are: " + str(len(event_list)) + " events to pick from. Yay!")
+        print("There are: " + str(len(event_list)) + " events to pick from.")
+        print("User event date: " + str(input_events[0].date))
+        print("Returned event date: " + str(event_list[0].date))
 
-    return event_list
+        for user_event in input_events:
+            event_found = find_one_event_from_inputs(event_list, user_event)
+            if event_found is not None:
+                desired_event_list.append(event_found)
+        print("-----------------------------------")
+    else:
+        print("Response code: " + str(response.status_code))
+    
+    return desired_event_list
 
-def format_columns(event_string_list):
-    event_list = []
-    # setup event string list
-    for row_string in event_string_list:
-        x = 0
-        # split string row to columns
-        column_list = list(row_string.split(","))
-        # clean up the header list
-        for column in column_list:
-            column_list[x] = column.strip()
-            x = x + 1
-        event_list.append(column_list)
-    return event_list
-
-def format_response_body(response_body):
-    # format payload
-    # find the right of this position
-    non_json_pay_load = response_body.find("aaData") + 8
-    # remove these things
-    json_pay_load = response_body[non_json_pay_load:]
-    json_pay_load = json_pay_load.rstrip("})")
-    json_pay_load = json_pay_load.replace("\n", "")
-    json_pay_load = json_pay_load.replace("\t", "")
-    json_pay_load = json_pay_load.replace("\t", "")
-    # add pipes for delimiting later
-    json_pay_load = json_pay_load.replace("],[", "|")
-    # remove these things
-    json_pay_load = json_pay_load.replace("[", "")
-    json_pay_load = json_pay_load.replace("]", "")
-    json_pay_load = json_pay_load.replace("&nbsp;", "")
-    json_pay_load = json_pay_load.replace("&nbsp;", "")
-    json_pay_load = json_pay_load.replace(
-        "<br><strong>Staff</strong><br />", "")
-    json_pay_load = json_pay_load.replace('"', "")
+def response_mapper(response_text):
+    # format for valid json
+    response_text = response_text.lstrip("( ").rstrip(" )").replace("&nbsp;", "")
+    json_obj = json.loads(response_text) 
 
     # create a list using pipes as delimiter
-    return list(json_pay_load.split("|"))
+    return list(json_obj["aaData"])
 
 def create_event_from_string(event_str_list):
     event_list = []
     for event_str in event_str_list:
-        new_event = Event()
-        new_event = new_event.create_from_group_ex_params(event_str)
+        new_event = event()
+        new_event = new_event.create_system_event(event_str)
         event_list.append(new_event)
     return event_list
 
 # find one event based on search criteria
-def find_one_event_from_inputs(availible_event_list, input_event):
+def find_one_event_from_inputs(availible_event_list, user_event):
     for event in availible_event_list:
-        if (
-            event.time == input_event.time
-            and event.title == input_event.title
-            and event.category == input_event.category
-            and event.studio == input_event.studio
-            and event.open_slots >= input_event.open_slots
-            and event.date == input_event.date
+        if (event.time == user_event.time
+            and event.title == user_event.title
+            and event.category == user_event.category
+            and event.studio == user_event.studio
         ):
-            return event
+            print("-----------------------------------")
+            print("Time, title, catagory, studio found")
+            if event.date == user_event.date:
+                print("Date found")
+                print("Slots availible: " + str(event.open_slots))
+                if event.open_slots >= user_event.open_slots:
+                    return event
 
-class Event:
-    def __init__(self):
-        self.day = ""
-        self.date = ""
-        self.time = ""
-        self.category = ""
-        self.studio = ""
-        self.open_slots = ""
-        self.location = ""
-        self.title = ""
-        self.year = ""
-        self.meta_data = ""
-        self.begin_date = ""
-        self.end_date = ""
-        self.sign_up_url = ""
-        self.status = ""
-        self.message = ""
-
-    def create_from_group_ex_params(self, event_str):
-        self.day = event_str[0]
-        self.month = event_str[1].split(" ")[0]
-        self.date = event_str[1].split(" ")[1]
-        self.year = event_str[2]
-        self.time = event_str[3]
-        self.title = event_str[4]
-        self.studio = event_str[6]
-        # compile the category
-        self.category = (
-            event_str[7]
-            + ", "
-            + event_str[8]
-            + ", "
-            + event_str[9]
-            + ", "
-            + event_str[10]
-        )
-        self.location = event_str[13]
-        # extract open_slots and Signup URL
-        meta_list = list(event_str[14].split("\\"))
-        if len(meta_list) > 12:
-            self.open_slots = meta_list[12].split(" ")[0]
-            self.sign_up_url = meta_list[16]
-
+class event:
+    def create_system_event(self, event_str):
+        self.date = event_str[0].split(" ")[2].rstrip(",")
+        self.time = event_str[1]
+        self.title = event_str[2]
+        self.studio = event_str[4]
+        self.category = event_str[5]
+        # extract sign_up_url and open_slots from the html tag
+        html_tag = event_str[9].split("=")
+        if len(html_tag) > 6:
+            self.open_slots = html_tag[6].split(" ")[0].split('"')[1]
+            self.sign_up_url = html_tag[8].split('"')[1]
         return self
 
-    def create_from_time(self, time):
-        self.category = category_description
-        self.studio = studio_description
-        self.open_slots = open_slots_number
-        self.title = event_title
-        self.time = time
+    def create_user_event(self, time):
         date = datetime.now() + time_offset
         self.date = str(date.day)
+        self.time = time
+        self.title = event_title
+        self.studio = studio_description
+        self.category = category_description
+        self.open_slots = open_slots_number
         return self
 
 if __name__ == "__main__":
